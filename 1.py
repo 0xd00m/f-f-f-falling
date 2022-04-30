25ab2bf9c9b357e3a589dc52ddde8f11078696c3
pd <xpd@duck.com>
2022-04-30 22:40:13 +0530

    Sat Apr 30 10:40:13 PM IST 2022

--- 1.py
+++ 1.py
@@ -30,12 +30,37 @@ attempts = 0
 # Print the length of the word
 print("The word is of length:", len(word), "and you have", len(word), "attempts to guess it.")
 
-while attempts < len(word):
+user_guess = "_" * len(word)
+
+# Convert user_guess to a list
+user_guess_list = list(user_guess)
+
+# Convert word to a list
+word_list = list(word)
+
+if attempts == 0:
+        print(user_guess_list)
+
+
+while attempts < len(word) and user_guess_list != word_list:
+    if user_guess_list == word_list:
+        print("\nYou guessed the word correctly!")
+        break
     # Ask the user to guess a letter    
     guess = input("Guess a letter: ")
     if guess in word:
         print("\nYes, the word contains the letter:", guess)
+        # Find the index of the letter in the word
+        index = word.index(guess)
+        # Print the index of the guessed letter
+        print("\nThe index of the letter is:", index)
+        # Replace the underscore with the letter
+        user_guess_list[index] = guess
+        print(user_guess_list)
+        
+
     else:
-        print("\nSorry, the word does not contain the letter:", guess)
+        print("\nYou have lost a life!. You have", len(word) - attempts, "attempts left.")
+        print(user_guess_list)
         attempts += 1
 

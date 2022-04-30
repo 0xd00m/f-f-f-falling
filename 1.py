243fd7f1fa9a9406e6ba2d152fedeee723ec4a40
pd <xpd@duck.com>
2022-04-30 22:11:04 +0530

    Sat Apr 30 10:11:04 PM IST 2022

--- 1.py
+++ 1.py
@@ -1,20 +1,41 @@
 # Generate a random word and ask the user to guess it.
 
 # import random library
-
 import random
 
-# Create a list of words
-words = ['apple', 'banana', 'orange', 'coconut', 'strawberry', 'lime', 'grapefruit', 'lemon', 'kumquat', 'blueberry', 'melon']
+# Create a tuple of words
+words = (
+    "apple",
+    "banana",
+    "orange",
+    "coconut",
+    "strawberry",
+    "lime",
+    "grapefruit",
+    "lemon",
+    "kumquat",
+    "blueberry",
+    "melon",
+)
 
+# Random word that will be guessed
 word = random.choice(words)
 
-print("This is the random word: ", word, ". Type of the word is: ", type(word))
+# Comment out later
+print("\nThis is the random word:", word +  ".\n")
 
-# find length of the word
-
-length = len(word)
+# Number of attempts the user has to guess the word starts with zero
+attempts = 0
 
 # Print the length of the word
+print("The word is of length:", len(word), "and you have", len(word), "attempts to guess it.")
 
-print("The length of the word is: ", length)
+while attempts < len(word):
+    # Ask the user to guess a letter    
+    guess = input("Guess a letter: ")
+    if guess in word:
+        print("\nYes, the word contains the letter:", guess)
+    else:
+        print("\nSorry, the word does not contain the letter:", guess)
+        attempts += 1
+
